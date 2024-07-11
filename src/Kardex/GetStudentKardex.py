61aec22d22def62703b77d53bd63d26e2c1dcaad
Gael González <gaeldev032@gmail.com>
2024-07-11 00:23:19 -0700

    Add: Kardex module & get kardex from curp

---
+++ src/Kardex/GetStudentKardex.py
@@ -0,0 +1,19 @@
+from src.config import Config
+import requests
+
+
+def GetStudentKardex(curp: str):
+    base_url = Config.read("Web", "kardex_url")
+    base_params = {
+        "curpAlumno": curp,
+        "clavePlantel": Config.read("School", "school_key"),
+        "turno": Config.read("School", "school_shift"),
+    }
+
+    response = requests.get(base_url, params=base_params)
+
+    return response
+
+
+if __name__ == "__main__":
+    pass
--- src/config.py
+++ src/config.py
@@ -3,32 +3,43 @@ import os
 
 default_config = {
     "General": {
+        "encoding": "utf-8",
+        "debug": True,
+    },
+    "Web": {
+        "kardex_url": "https://apps.cobachbcs.edu.mx/Sice/ReportesImpresos/wf_Rep_Kardex_ws.aspx",
+    },
+    "School": {
+        "school_key": "03ECB0004K",
+        "school_shift": "M",
     }
 }
 
 
-def create():
+class Config():
 
-    print("Creating config file")
-    try:
+    def create():
+        print("Creating config file")
+        try:
+            config = configparser.ConfigParser()
+            config["General"] = default_config.get("General")
+            config["Web"] = default_config.get("Web")
+            config["School"] = default_config.get("School")
+            with open("config.ini", "w") as config_file:
+                config.write(config_file)
+            print("Config file has been written successfully")
+        except Exception as e:
+            print("Error")
+            print(e)
+
+    def read(section: str, option: str):
+        if not os.path.isfile("config.ini"):
+            print("config.ini not found")
+            create()
         config = configparser.ConfigParser()
-        config["General"] = default_config.get("General")
-        with open("config.ini", "w") as config_file:
-            config.write(config_file)
-        print("Config file has been written successfully")
-    except Exception as e:
-        print("Error")
-        print(e)
-
-
-def read(section: str, option: str):
-    if not os.path.isfile("config.ini"):
-        print("config.ini not found")
-        create()
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    return config.get(section, option)
+        config.read("config.ini")
+        return config.get(section, option)
 
 
 if __name__ == "__main__":
-    create()
+    Config.create()

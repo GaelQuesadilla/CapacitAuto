58f8e1b54e0b01c7814e194be92fcb233abc1c3e
Gael González <gaelglz032@gmail.com>
2024-10-13 22:43:35 -0600

    Adding perfect lists!! finally :DD

--- src/Lists/AdvancedGroup.py
+++ src/Lists/AdvancedGroup.py
@@ -1,13 +1,15 @@
 from src.Lists.Group import Group
 from src.Lists.Student import Student
 from src.Lists.StudentList import StudentList
-from typing import List, Dict
+from typing import List, Dict, Any
 from dataclasses import dataclass
 import pandas as pd
 from src.Config import Config
-from src.Log import Log
+from src.Log import Log, PrintLog
 
-maxStudents = Config.read("School", "max_students_in_group")
+maxStudents: int = Config.read("School", "max_students_in_group")
+optionPrefix: str = Config.read("General", "choice_name")
+relevantGradesPrefix: str = Config.read("General", "relevant_grades_name")
 
 
 class AdvancedGroup(Group):
@@ -25,8 +27,9 @@ class AdvancedGroup(Group):
             self.courses = Config.read("School", "trainings").split(",")
             self.group = "Paquetes"
         else:
-            Log.log(
-                "Advanced groups should be used only in 1,2,3,4 semesters", Log.warning)
+            PrintLog.warning(
+                "Advanced groups should be used only in 1,2,3,4 semesters"
+            )
 
         groups: Dict[str, StudentList] = {}
 
@@ -42,3 +45,70 @@ class AdvancedGroup(Group):
     @property
     def allStudents(self):
         return self._allStudents
+
+    def setPerfectLists(self):
+        for course in self.courses:
+            currentList = self._studentLists.get(course)
+            studentsData: List[Dict[str, Any]] = self.allStudents.loc[
+                self.allStudents[optionPrefix.format(course)] == 1
+            ].to_dict(orient="records")
+
+            for studentData in studentsData:
+                currentStudent = Student(Semestre=self.semester)
+                for key, value in studentData.items():
+                    currentStudent.set(key, value)
+
+                currentStudent.Semestre = str(currentStudent.Semestre)
+                currentList.addStudent(
+                    student=currentStudent)
+
+        self.sortByGrades()
+
+    def sortByGrades(self):
+        for course in self.courses:
+            currentList = self._studentLists[course]
+            currentList.sort(ascending=False, by=[
+                             "Promedio", relevantGradesPrefix.format(course)])
+
+
+if __name__ == "__main__":
+    studentListA = StudentList(
+        fileName="data\\lists\\TEST\\Lista Alumnos 2-A.xlsx",
+        group="A",
+        semester="2"
+    )
+    studentListA.load()
+
+    studentListB = StudentList(
+        fileName="data\\lists\\TEST\\Lista Alumnos 2-B.xlsx",
+        group="B",
+        semester="2"
+    )
+    studentListB.load()
+
+    studentListC = StudentList(
+        fileName="data\\lists\\TEST\\Lista Alumnos 2-C.xlsx",
+        group="C",
+        semester="2"
+    )
+    studentListC.load()
+
+    studentListD = StudentList(
+        fileName="data\\lists\\TEST\\Lista Alumnos 2-D.xlsx",
+        group="D",
+        semester="2"
+    )
+    studentListD.load()
+
+    advancedGroup = AdvancedGroup(
+        "2", studentListA, studentListB, studentListC, studentListD)
+
+    PrintLog.info("All students")
+    print(f"{advancedGroup.allStudents}")
+
+    PrintLog.info("Set Perfect List")
+    advancedGroup.setPerfectLists()
+
+    for course, studentList in advancedGroup.studentLists.items():
+        PrintLog.info(f"Lista - {course}")
+        print(studentList.df)

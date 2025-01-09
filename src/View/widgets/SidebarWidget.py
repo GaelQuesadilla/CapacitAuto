f461e1153596e0c0a1f9b8de58e75f7c3854cd44
Gael González <gaeldev032@gmail.com>
2025-01-09 16:40:31 -0700

    Refactor SidebarWidget to manage dynamic button creation and widget loading with improved logging

--- src/View/widgets/SidebarWidget.py
+++ src/View/widgets/SidebarWidget.py
@@ -1,5 +1,16 @@
 import tkinter as tk
 from src.View.MainApp import MainApp
+from typing import Dict, Callable, Union, List
+from dataclasses import dataclass
+from src.Log import setup_logger
+
+logger = setup_logger()
+
+
+@dataclass
+class currentWidget:
+    name: str
+    widget: tk.Widget
 
 
 class SidebarWidget(tk.Frame):
@@ -7,29 +18,59 @@ class SidebarWidget(tk.Frame):
         super().__init__(master)
 
         self.configure(
-            width=400, bg="#2c3e50",
+            width=500, bg="#2c3e50",
             relief="sunken", padx=20, pady=10
         )
 
-        self.buttons = ["Home", "Settings", "Profile", "Help", "Exit"]
-        for button_name in self.buttons:
-            button = tk.Button(
-                self,
-                text=button_name,
-                bg="#34495e",
-                fg="white",
-                relief="flat",
-                command=lambda name=button_name: self.on_button_click(name)
-            )
-            button.pack(fill="x", pady=5, padx=10)
+        self.buttonData: Dict[str, Dict[str, Union[Callable, tk.Button]]] = {}
 
-    def on_button_click(self, button_name):
-        print(f"{button_name} button clicked")
+        self.currentWidget: currentWidget = currentWidget(None, None)
+
+    def onClick(self, name: str):
+
+        if self.currentWidget.name == name:
+            return
+        if not self.currentWidget.widget is None:
+            self.currentWidget.widget.destroy()
+
+        newWidget = self.buttonData.get(name).get("widget", None)()
+        self.currentWidget = currentWidget(name, newWidget)
+
+        logger.info(f"Loading '{name}' Widget as view")
+        self.currentWidget.widget.pack(fill=tk.BOTH, expand=True)
+
+    def addButton(self, name: str, widget: Callable, onClick=None):
+
+        if onClick is None:
+            def onClick(): return self.onClick(name)
+
+        button = tk.Button(
+            self,
+            text=name,
+            bg="#34495e",
+            fg="white",
+            relief=tk.FLAT,
+            command=lambda: onClick()
+        )
+        button.pack(fill=tk.X, pady=3, padx=20)
+
+        self.buttonData[name] = {"widget": widget, "button": button}
 
 
 if __name__ == "__main__":
-    app = MainApp()
-    sidebar = SidebarWidget(app)
 
+    app = MainApp()
+
+    sidebar = SidebarWidget(app)
     sidebar.pack(side=tk.LEFT, fill=tk.Y)
+    i = 0
+
+    def red(): return tk.Frame(app, bg="red")
+    def blue(): return tk.Frame(app, bg="blue")
+    def purple(): return tk.Frame(app, bg="purple")
+
+    sidebar.addButton("red", red)
+    sidebar.addButton("blue", blue)
+    sidebar.addButton("purple", purple)
+
     app.mainloop()
